ca48425a7f3d863d403f776f2a629ba14adcf030
loris pistilli <loris.pistilli@edu.esiee.fr>
2022-05-26 23:43:44 +0200

    basics movement are implemented but feels unnatural, need more research on how to make it more natural maybe use an asset like <SUPER Character Controller>

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,16 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
-
     public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    
     private Vector3 _velocity;
+    private bool isGrounded;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +26,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
         var x = Input.GetAxis("Horizontal");
         var z = Input.GetAxis("Vertical");
 
@@ -28,6 +41,11 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * (speed * Time.deltaTime));
 
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            _velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        
         _velocity.y += gravity * Time.deltaTime;
 
         controller.Move(_velocity * Time.deltaTime);
