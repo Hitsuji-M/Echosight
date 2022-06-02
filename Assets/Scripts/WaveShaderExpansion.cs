f4a0bc62685fcd831f203b025e99f6640e5eace1
Franck Jiang <franck.jiang@outlook.fr>
2022-06-02 12:34:11 +0200

    Revert "merge commit wave-main"
    This reverts commit ae0873bfee3e615341e67f1577d5ff4f2a8cba6a, reversing
    changes made to 737ef080a90fad19d0098653bbf7554e527fae6c.

--- Assets/Scripts/WaveShaderExpansion.cs
+++ Assets/Scripts/WaveShaderExpansion.cs
@@ -7,27 +7,23 @@ public class WaveShaderExpansion : MonoBehaviour
     public Color waveColor;
     public float impactStrength;
     public float soundSharpness;
-    public Material[] material;
+    public Material material;
     public Vector4[] waveOrigin;
 
     private static readonly Vector4[] emptyVector = new Vector4[] { new Vector4(0, 0, 0, 1) };
     private int waveIndex;
-    private int nMaterial;
 
     void Start(){
         waveOrigin = new Vector4[10];
         waveIndex = 0;
-        nMaterial = material.Length;
         for (int i = 0; i < 10; i++) 
         {
             waveOrigin[i] = new Vector4(0, 0, 0, 1);
         }
         Vector2 waveParam = new Vector2(impactStrength, soundSharpness);
-        for (int i = 0; i < nMaterial; i++)
-        {
-            material[i].SetVector("_WaveParam", waveParam);
-            material[i].SetColor("_Color", waveColor);
-        }
+
+        material.SetVector("_WaveParam", waveParam);
+        material.SetColor("_Color", waveColor);
     }
     public void Spawn(Vector3 spawnPoint) 
     {
@@ -38,18 +34,13 @@ public class WaveShaderExpansion : MonoBehaviour
     void Update()
     {
         int nbWaves = waveOrigin.Length;
-        for (int i = 0; i < nMaterial; i++) {
-            material[i].SetInt("nbWave", nbWaves);
-        }
+        material.SetInt("nbWave", nbWaves);
         for (int i = 0; i < nbWaves; i++) {
             waveOrigin[i] = new Vector4(waveOrigin[i].x,
                                         waveOrigin[i].y,
                                         waveOrigin[i].z,
                                         Mathf.Min(waveOrigin[i].w + Time.deltaTime/4, 1));
-            for (int j = 0; j < nMaterial; j++)
-            {
-                material[j].SetVectorArray("_WaveOrigin", waveOrigin);
-            }
+            material.SetVectorArray("_WaveOrigin", waveOrigin);
         }
         //Debug.Log(material.GetVectorArray("_WaveOrigin")[1]);
         //Debug.Log(material.GetVectorArray("_WaveParam")[0]);
