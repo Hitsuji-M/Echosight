02c24c2b5595a764bcb67e6fafa976bed9c21514
AstridWinkler <astrid17.winkler@gmail.com>
2022-06-16 10:56:25 +0200

    random water drop audio

--- Assets/Scripts/Environnement/WaterDrop.cs
+++ Assets/Scripts/Environnement/WaterDrop.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 
 public class  WaterDrop : MonoBehaviour
 {
-    // Spawns waves at regular intervals and play water drop sound (to be implemented)
+    // Spawns waves at regular intervals and play water drop sound
   
     private GameObject _waveCtrl;
-
+    [SerializeField] AudioClip[] _clips;
 
     void Start()
     {
@@ -21,8 +21,13 @@ public class  WaterDrop : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(5);
+            float delay = UnityEngine.Random.Range(5, 10);
+            yield return new WaitForSeconds(delay);
+
             _waveCtrl.GetComponent<WaveShaderExpansion>().Spawn(new Vector3(transform.position.x,transform.position.y+1, transform.position.z), 4);
+            int index = UnityEngine.Random.Range(0, _clips.Length);
+            AudioClip clip = _clips[index];
+            GetComponent<AudioSource>().PlayOneShot(clip);
         }
     }
 }
